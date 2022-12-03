47e0274b269e4783c2e2d29e17899233bf1572b3
Manoel Vilela <manoel_vilela@engineer.com>
2022-12-03 20:20:16 -0300

    Fix copyright name

--- 2022/day01/main.go
+++ 2022/day01/main.go
@@ -1,4 +1,4 @@
-// Copyright 2022 the adventofcode Authors. All rights reserved
+// Copyright 2022 to ryukinix. All rights reserved
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
