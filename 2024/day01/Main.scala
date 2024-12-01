5db2dcf22d02d35ff9674ab9529c2eb72ecb1645
Manoel Vilela <manoel.neto@neoway.com.br>
2024-12-01 19:49:54 -0300

    Add part b of day01

--- 2024/day01/Main.scala
+++ 2024/day01/Main.scala
@@ -5,8 +5,9 @@ object Main extends App {
   val input = inputPairNumbers()
   val distancesSum = minimumPairDistanceSum(input)
 
-  println(s"Input[0:10]: ${input.slice(0, 10)} ...")
-  println(s"distanceSum: ${distancesSum}")
+  println(s"Input[0:10]: ${input.slice(0, 10)} ...\n")
+  println(s"part(a) | distanceSum: ${distancesSum}")
+  println(s"part(b) | similaritySum: ${similaritySum(input)}")
 
   def inputPairNumbers(): List[(Int, Int)] = {
     val fileName = "input.txt"
@@ -15,6 +16,14 @@ object Main extends App {
         case Array(first, second) => (first, second)
       }
     }
+  }
+
+  def similaritySum(pairNums: List[(Int, Int)]): Int = {
+    val numbers = pairNums.map(_._2)
+
+    // Using groupBy and mapValues
+    val frequencyMap = numbers.groupBy(identity).mapValues(_.size)
+    pairNums.map(_._1).map(x => x * frequencyMap.getOrElse(x, 0)).sum()
   }
 
   def minimumPairDistanceSum(pairNums: List[(Int, Int)]): Int = {
