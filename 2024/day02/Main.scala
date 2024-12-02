ae59050bb373a05e8e7fc66d75f178f9f8c20f37
Manoel Vilela <manoel.neto@neoway.com.br>
2024-12-02 11:05:58 -0300

    Add part b solution

--- 2024/day02/Main.scala
+++ 2024/day02/Main.scala
@@ -4,14 +4,23 @@ import scala.math.{signum, abs}
 object Main extends App {
   val numbers = inputNumbers()
   val safeLines = numbers.count(safeLine)
+  val safeLinesTolerant = numbers.count(safeLineTolerant)
   // numbers.filter(safeLine).map(println)
   // println(safeLine(List(17, 18, 11, 8, 3)))
   println(s"part(a) | safeLines: ${safeLines}")
+  println(s"part(b) | safeLinesTolerant: ${safeLinesTolerant}")
 
   def inputNumbers(): List[List[Int]] = {
     val fileName = "input.txt"
     Source.fromFile(fileName).getLines()
           .map(_.split("\\s+").map(_.toInt).toList).toList
+  }
+
+  def safeLineTolerant(line: List[Int]): Boolean = {
+    val linesWithoutOneLevel = line.zipWithIndex.map {
+      case (_, idx) => line.patch(idx, Nil, 1)
+    }
+    linesWithoutOneLevel.exists(safeLine)
   }
 
   def safeLine(line: List[Int]): Boolean = {
