4a32ac32bddb01483a376bd44cbdde9511f456c3
Manoel Vilela <manoel.neto@neoway.com.br>
2024-12-06 09:30:02 -0300

    Solve part A using recursion

--- 2024/day05/Main.scala
+++ 2024/day05/Main.scala
@@ -1,15 +1,38 @@
 import scala.io.Source
+import scala.annotation.tailrec
 
 case class PageData(
   pageOrderingRules: List[(Int, Int)],
-  pageNumbers: List[List[Int]]
+  updatePageNumbers: List[List[Int]]
 )
 
 
 object Main extends App {
-
   // val input = parseInput()
-  println(inputTest())
+  //println(inputTest())
+  test1()
+  val input = parseInput()
+  val result = sumMiddleNumberOfValidPageNumbers(input)
+  println(s"part(a) | sumMiddleNumberOfValidPageNumbers: ${result}")
+
+  @tailrec
+  def checkValidPageNumbers(pageOrderingRules: List[(Int, Int)], pageNumbers: List[Int]): Boolean = {
+    pageNumbers match {
+      case Nil => true
+      case head :: tail => {
+        tail.forall(n => pageOrderingRules.exists(p => p == (head, n))) && checkValidPageNumbers(pageOrderingRules, tail)
+      }
+    }
+  }
+
+  def sumMiddleNumberOfValidPageNumbers(pageData: PageData): Int = {
+    pageData.updatePageNumbers
+      .filter {
+        pageNumbers => checkValidPageNumbers(pageData.pageOrderingRules, pageNumbers)
+      }.map {
+        pageNumbers => pageNumbers(pageNumbers.size / 2)
+    }.sum()
+  }
 
   def parseInput(): PageData = {
     val fileName = "input.txt"
@@ -27,8 +50,15 @@ object Main extends App {
     val pageNumbers = secondSection.tail.map(_.split(",").map(_.toInt).toList).toList
     PageData(
       pageOrderingRules = pageOrderingRules,
-      pageNumbers = pageNumbers
+      updatePageNumbers = pageNumbers
     )
+  }
+
+  def test1() = {
+    val expectedResult = 143
+    val testResult     = sumMiddleNumberOfValidPageNumbers(inputTest())
+    val status         = if (testResult == expectedResult) "passed" else "failed"
+    println(s"test1[${status}]: got ${testResult}, expected ${expectedResult}")
   }
 
   def inputTest(): PageData = {
