bfc564fbdf5ceb59a7956c9fddab7e0c758bb1aa
Manoel Vilela <manoel.neto@neoway.com.br>
2025-12-06 23:15:54 -0300

    test: make easier to debug

--- 2025/day01/main.lisp
+++ 2025/day01/main.lisp
@@ -11,6 +11,12 @@
     ("R14" . 14)
     ("L82" . 32)))
 
+(defparameter *test-input-extra*
+  '("L150"
+   "L50"
+   "L150"
+   "R50"))
+
 (defun parse-rotation (rot-string)
   (let* ((head (char rot-string 0))
          (tail (subseq rot-string 1))
@@ -24,10 +30,11 @@
   (let ((r (+ rotation pointer)))
     (cond
       ((< r 0) (values (mod (+ r 100) 100)
-                       (1+ (truncate (abs r) 100))))
+                       (+ (if (> pointer 0) 1 0)
+                          (truncate (abs (+ r 100)) 100))))
       ((>= r 100) (values (mod r 100)
                           (truncate r 100)))
-      (t (values r 0)))))
+      (t (values r (if (eq r 0) 1 0))))))
 
 (defun apply-rotation (rot-strings initial-position)
   (loop :with p = initial-position
@@ -40,27 +47,38 @@
         :for rot-string in rot-strings
         :for (new-p rotations) = (multiple-value-list (rotate (parse-rotation rot-string) p))
         :do (setq p new-p)
-        :sum rotations))
+        :collect rotations))
+
+(defun sum (l)
+  (reduce #'+ l :initial-value 0))
+
+(defun count-rotation-with-interlap (rot-strings initial-position)
+  (sum (apply-rotation-with-interlap rot-strings initial-position)))
+
 
 (defun run-tests ()
   (let* ((rot-strings (mapcar 'car *test-input*))
          (expected (mapcar 'cdr *test-input*))
          (rotations (apply-rotation rot-strings *pointer*))
          (count-zeros (count 0 rotations))
-         (interlap-zeros (apply-rotation-with-interlap rot-strings *pointer*))
+         (interlap-zeros (count-rotation-with-interlap rot-strings *pointer*))
+         (extra-test (count-rotation-with-interlap *test-input-extra* *pointer*))
          (check-rotations (loop :for rotation in rotations
                                 :for rotation-expected in expected
                                 :always (= rotation rotation-expected))))
     (assert check-rotations (rotations) "Rotations: ~a" rotations)
     (assert (eq count-zeros 3) (count-zeros) "Expected 3 zeros, but found ~a" count-zeros)
-    (assert (eq interlap-zeros 6) (interlap-zeros) "Expected 6 interlap-zeros, but found ~a" interlap-zeros)))
+    (assert (eq interlap-zeros 6) (interlap-zeros) "Expected 6 interlap-zeros, but found ~a" interlap-zeros)
+    ;(assert (eq extra-test 4) (extra-test) "Expected 4 in extra-test, but found ~a" extra-test)
+    )
+  )
 
 (defun solve-part-a (lines)
   (let ((zeros (count 0 (apply-rotation lines *pointer*))))
     (format t "Part A: ~a~%" zeros)))
 
 (defun solve-part-b (lines)
-  (let ((zeros (apply-rotation-with-interlap lines *pointer*)))
+  (let ((zeros (count-rotation-with-interlap lines *pointer*)))
     (format t "Part B: ~a~%" zeros)))
 
 (defun main ()
